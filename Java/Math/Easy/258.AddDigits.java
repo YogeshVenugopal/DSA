2be6a0a03af5524943b052df4f4a9e2f45b14571
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-02-17 22:12:47 +0530

    Runtime : 1ms /| Beats : 97.66% with time complexity - O(log n) and space complexity - O(1)

--- Java/Math/Easy/258.AddDigits.java
+++ Java/Math/Easy/258.AddDigits.java
@@ -1,20 +1,25 @@
 
 class Solution {
     public int addDigits(int num) {
+
         // Create a loop and it's run until the number become single digit
         while(num >= 10){
+            
             // Declare the variable for the mathematic opertions
             int sum = 0;
+
             // Adding another loop for sum the last digit in the number
             while(num > 0){
                 sum += num % 10;
                 num /= 10;
             }
+
             // Set the sum value as current number
             // Case - 1: if the number is still two digit then it goes once again
             // Case - 2: if the number is comes to single digit then it exit from the loop
             num = sum;
         }
+
         // return the single digit value 
         return num;
     }
