ccf3dc6bea19beded83e9a094b0bf4efe3ec730a
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-05-10 12:00:00 +0530

    solution: assign cookie using greedy algorithm

---
+++ problems/Java/Arrays/Easy/AssignCookies.java
@@ -0,0 +1,21 @@
+package problems.Java.Arrays.Easy;
+
+class AssignCookies {
+    public static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);   
+        Arrays.sort(s);
+
+        int i = 0, j = 0;
+        while(i < g.length && j < s.length){
+            if(g[i] <= s[j]) i++;
+            j++;
+        }   
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] g = {1, 2, 3};
+        int[] s = {1, 1};
+        System.out.println("Number of content children: " + findContentChildren(g, s)); // Output: 1
+    }
+}
