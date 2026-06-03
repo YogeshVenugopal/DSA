0271034c5ba0494dcc5f3fb222f72f7cb9e4457a
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-06-03 20:55:59 +0530

    solution: best time to sell or buy in java

---
+++ problems/Java/Arrays/BestTimeToSellOrBuy.java
@@ -0,0 +1,20 @@
+package problems.Java.Arrays;
+
+public class BestTimeToSellOrBuy {
+
+    public static int maxProfit(int[] stocks) {
+        int currentSum = 0;
+        int maxSum = 0;
+
+        for (int i : stocks) {
+            currentSum = Math.min(i, currentSum);
+            maxSum = Math.max(maxSum, i - currentSum);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] stocks = {2, 4, 1};
+        System.out.println(maxProfit(stocks));
+    }
+}
